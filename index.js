20b5a628a54252bd97cbc307118a8e7a8849e3ae
Rich Hildred <rhildred@gmail.com>
2023-07-15 08:04:29 -0400

    fix date in example

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import SQLite from 'sqlite3';
 
-
 async function main() {
   const sequelize = new Sequelize('database', 'username', 'password', {
     dialect: 'sqlite',
@@ -11,7 +10,7 @@ async function main() {
       // for instance, this is how you can configure the database opening mode:
       mode: SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE | SQLite.OPEN_FULLMUTEX,
     },
-  });  
+  });
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
     birthday: DataTypes.DATE,
@@ -19,7 +18,7 @@ async function main() {
   await sequelize.sync();
   const jane = await User.create({
     username: 'janedoe',
-    birthday: new Date(1980, 6, 20),
+    birthday: new Date('1980-06-20'),
   });
 
   const users = await User.findAll();
